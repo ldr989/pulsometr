90e260229aedcc6f41cbd8971a13bcbb8e478f50
Ldr <89.freedom@gmail.com>
2022-06-04 10:58:41 +0600

    form validation and input mask completed

--- src/js/sript.js
+++ src/js/sript.js
@@ -52,5 +52,36 @@ $(function() {
         });
     });
 
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name:{
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Минимум {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Введите свой электронный адрес",
+                    email: "Неправильно введен электроный адрес"
+                }
+            }
+        });
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form')
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 }); 
 })(jQuery);
