4dafd8af134b7ad8b0f7c3c599c3e1222ebb39c0
Ldr <89.freedom@gmail.com>
2022-06-05 12:56:49 +0600

    added some changes in js file

--- src/js/sript.js
+++ src/js/sript.js
@@ -88,7 +88,7 @@ $(function() {
         e.preventDefault();
         $.ajax({
             type: "POST",
-            url: "../mailer/smart.php",
+            url: "mailer/smart.php",
             data: $(this).serialize()
         }).done(function() {
             $(this).find("input").val("");
